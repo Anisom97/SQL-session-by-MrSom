bdfcf8d5afe306de747733a10f9abdce0a54dbe5
Anisom97 <52325183+Anisom97@users.noreply.github.com>
2023-12-18 22:48:58 +0530

    Add files via upload

---
+++ TEST FILE.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Persons (
+    EMPID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    EMAIL varchar(255),
+    PHONE varchar(255),
+	HIREDATE varchar(255),
+	SALARY int
+);
+
+INSERT INTO Persons 
+VALUES 
+('0001','Kevin','Weiss','kevin@test.com','123.123.12','08-10-2019',17000),
+('0002','Lex','De Haan','lex@test.com','123.123.13','05-05-2019',15000),
+('0003','Laura','Bissot','laura@test.com','123.123.15','02-08-2019',40000);
